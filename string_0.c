8aaf536fc5a881c24f64de3785578f27f19a7960
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-05-12 10:31:58 -0700

    strings

---
+++ string_0.c
@@ -0,0 +1,45 @@
+#include "shell.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: string whose length is to be determined
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strcmp - function that compares two strings
+ * @s1: the first string
+ * @s2: the second sring
+ *
+ * Return: negative if s1 is less than s2, positive if
+ * s1 is greater than s2, or zero if s1 = s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
+
---
+++ string_1.c
@@ -0,0 +1,37 @@
+#include "shell.h"
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: destination buf
+ * @src: source buf
+ *
+ * Return: the concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *k = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (k);
+}
+
+/**
+ * start_hays_need - function that checks if needle starts with
+ * haysatck
+ * @needle: substring
+ * @haystack: string to search
+ *
+ * Return: the address of the next character in the haystack
+ * string or NULL
+ */
+char *start_hays_need(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
