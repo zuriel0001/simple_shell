72912963264e9852c71082b5793568d202d37808
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-05-13 06:31:39 -0700

    remove comment

---
+++ 15_comment_handler.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+
+/**
+ * erase_comments - a function that ensure comments are not compiled
+ * Description:  it replces first occurance of '#' with a '\0'
+ *
+ * @buf: address of the string to modify
+ *
+ * Return: nothing
+ */
+
+void erase_comments(char *buf)
+{
+	int index = 0;
+
+	while (buf[index] != '\0')
+	{
+		if (buf[index] == '#' && (index == 0 || buf[index - 1] == ' '))
+		{
+			buf[index] = '\0';
+			break;
+		}
+		index++;
+	}
+}
+
