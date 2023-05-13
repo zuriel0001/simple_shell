aced116ad400600de1fd720f00914cfcaa0ba328
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-05-13 10:12:21 -0700

    string_2

---
+++ string_2.c
@@ -0,0 +1,42 @@
+#include "shell.h"
+
+/**
+ * _strcpy - function that copies a string to src
+ * @src: the source
+ * @dest: the destination file
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
+/**
+ * _puts - a function that prints a string
+ * @str: the string to be printed
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
