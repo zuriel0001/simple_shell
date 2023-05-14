383d47a448c10011cc688d4b7a68e21514a45359
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-05-14 08:50:55 -0700

    exit shell

---
+++ 4_exit_builtin.c
@@ -0,0 +1,34 @@
+#include "shell.h"
+
+/**
+ * exit_shell - a function that exits the shell
+ * 
+ * @info: a pointer to a structure of type shell_info
+ *
+ * Return: 1 if error occur, 0 if info.argv[0] != "exit"
+ */
+
+int exit_shell(shell_info *info)
+{
+	int check_exit;
+	
+	/* if there is an exit arguement */
+	if (info->argv[1])
+	{
+		check_exit = atoi_overflow(info->argv[1]);
+
+		if (check_exit == -1)
+		{
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			puts_error(info->argv[1]);
+			putchar_error('\n');
+			return (1);
+		}
+		info->err_num = atoi_overflow(info->argv[1]);
+		return (-2);
+	}
+	info->err_num = -1;
+	return (-2);
+}
+
