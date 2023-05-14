f85929788c2b088ee89749a73aec1a5573006888
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-05-14 08:51:27 -0700

    check for interactive mode

---
+++ interactive_shell.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+/**
+ * interactive_mode - a dfunction tells if a the shell is in interactive mode
+ *
+ * @info: pointer to a structure
+ *
+ * Return: 1 if in interactive mode, otherwise 0
+ */
+
+int interactive_mode(shell_info *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
