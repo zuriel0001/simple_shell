31525da144006344eaeed185050a7ba70241db3a
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-05-18 04:49:10 -0700

    list string

--- list_list.c
+++ list_list.c
@@ -23,3 +23,44 @@ size_t print_list(const list_t *h)
 	}
 	return (j);
 }
+
+/**
+ * list_str - function that returns an array of string to the list
+ * @h: pointer to the first node
+ *
+ * Return: the array of string
+ */
+char **list_str(list_t *h)
+{
+	list_t *n = h;
+	size_t i = list_length(h), k;
+	char **sss;
+	char *str;
+
+	if (!h || !i)
+		return (NULL);
+	sss = malloc(sizeof(char *) * (i + 1));
+
+	if (!sss)
+		return (NULL);
+
+	for (i = 0; n; n = n->next, i++)
+	{
+		str = malloc(_strlen(n->str) + 1);
+		if (!str)
+		{
+			k = 0;
+			while (k < i)
+			{
+				free(sss[k]);
+				k++;
+			}
+			free(sss);
+			return (NULL);
+		}
+		str = _strcpy(str, n->str);
+		sss[i] = str;
+	}
+	sss[i] = NULL;
+	return (sss);
+}
