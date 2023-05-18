f80a6eed85327b5557ef90b269d93ed48c472c0f
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-05-18 06:36:44 -0700

    resize a previously allocated block of memory

---
+++ memory_utils.c
@@ -0,0 +1,44 @@
+#include "shell.h"
+
+/**
+ * resize_mem - a function that resize a previously allocated block of memory
+ *
+ * @p_mem: pointer to previous allocated block or mem. using malloc
+ * @old_size: bytes of previewsly allocated memory
+ * @new_size: bytes of newly allocated memory
+ *
+ * Return: pointer to the newly allocated memory block
+ */
+
+void *resize_mem(void *p_mem, unsigned int old_size, unsigned int new_size)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (!p_mem)
+		return (malloc(new_size));
+
+	if (!new_size)
+		return (free(p_mem), NULL);
+
+	if (new_size == old_size)
+		return (p_mem);
+
+	ptr = malloc(new_size);
+	if (!ptr)
+		return (NULL);
+
+	if (old_size < new_size)
+		old_size = old_size;
+	else
+		old_size = new_size;
+
+	for (i = 0; i < old_size; i++)
+	{
+		ptr[i] = ((char *)p_mem)[i];
+	}
+
+	free(p_mem);
+	return (ptr);
+}
+
