8fe8886c129536678dd755a899bcaccfd777864d
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-05-21 06:50:30 -0700

    added alias remover

--- alias_history_utils.c
+++ alias_history_utils.c
@@ -14,3 +14,41 @@ int show_history(shell_info *info)
 
 	return (0);
 }
+
+
+
+/**
+ * remove_alias - function removes an alias from the list of aliases
+ *
+ * @info: pointer to a structure that contains information about the aliases
+ * @str: A pointer to a string that contains the alias to be unset
+ *
+ * Return:  0 on success, otherwise 1 on error
+ */
+
+int remove_alias(shell_info *info, char *str)
+{
+	char *ptr; /* A pointer to a character in the string str */
+	char saved_char; /* temporarily store the value of the '=' character in str */
+	int ret; /* store the return value of the "remove_node_at_idx()" function */
+	int node_ind;
+
+	ptr = _strchr(str, '=');
+
+	if (ptr == NULL)
+		return (1);
+
+	saved_char = *ptr;
+	*ptr = '\0';
+
+	node_ind = fetch_node_idx(info->alias, start_node_with(info->alias, str, -1));
+	ret = remove_node_at_idx(&(info->alias), node_ind);
+
+	/**
+	* ret = remove_node_at_idx(&(info->alias), fetch_node_idx(info->alias,
+	* node_starts_with(info->alias, str, -1)));
+	*/
+
+	*ptr = saved_char;
+	return (ret);
+}
