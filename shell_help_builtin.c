0c7cf008dd6069cc5049f1cfaa804942665afa01
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-05-23 13:45:02 -0700

    fixing error

--- shell_help_builtin.c
+++ shell_help_builtin.c
@@ -15,32 +15,14 @@
 
 int helper(shell_info *info)
 {
-	UNUSED(info);
+	char **arg_arr;
 
-	_puts("Shell Commands:\n");
-	_puts("  help     - Display this help information.\n");
-	_puts("  cd       - Change the current working directory.\n");
-	_puts("  pwd      - Display the current working directory.\n");
-	_puts("  env      - Display a list of environment variables.\n");
-	_puts("  setenv   - Set the value of an environment variable.\n");
-	_puts("  unsetenv - Remove an environment variable.\n");
-	_puts("  exit     - Exit the shell.\n");
-	_puts("\n");
-	_puts("Usage:\n");
-	_puts("  To execute a command, simply type the name of the command\n");
-	_puts("  followed by any arguments, separated by spaces.\n");
-	_puts("\n");
-	_puts("  To navigate the file system, use the 'cd' command.\n");
-	_puts("  For example, 'cd /usr/bin' will change the current\n");
-	_puts("  working directory to '/usr/bin'.\n");
-	_puts("\n");
-	_puts("  To set an environment variable, use the 'setenv' command.\n");
-	_puts("  For example, 'setenv MY_VAR my_value' will set the value of\n");
-	_puts("  the 'MY_VAR' environment variable to 'my_value'.\n");
-	_puts("\n");
-	_puts("  To remove an environment variable, use the 'unsetenv' command.\n");
-	_puts("  For example, 'unsetenv MY_VAR' will remove the 'MY_VAR'\n");
-	_puts("  environment variable.\n");
-	_puts("\n");
+	arg_arr = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+
+	if (0)
+	{
+		_puts(*arg_arr);
+	}
 	return (0);
 }
